2bf65c29b842c20567ac93368e5861e88fb0fb45
manoj <manojpoojary2004@gmail.com>
2023-04-06 14:57:38 +0530

    style change

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var f1= document.querySelector('.f1')
 var f2= document.querySelector('.f2')
 var out=document.querySelector('.out')
-// var button=document.querySelector('.button')
+var btn=document.querySelector('.collect')
 var button=document.querySelectorAll('.b')
+// alert("Please enter number")
 
 // var sum=eval("2+2");
 // console.log(sum);
@@ -14,12 +15,15 @@ var button=document.querySelectorAll('.b')
 //  console.log(collect)
  
 // }
-
+f1.addEventListener("Change",()=>{
+  alert("This is message")
+})
 for(var i=0;i<button.length;i++)
 {
 button[i].addEventListener("click",function fun(event)
 {
   var op=event.currentTarget.textContent
+  btn.value=op
   console.log(op);
   var string=f1.value+op+f2.value;
   console.log(eval(string));
